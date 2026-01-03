630cb7c8c175ffd03977c46224da52f2be3d1f62
k23136072 <hissan.omar@kcl.ac.uk>
2026-01-03 20:19:05 +0400

    Implement looping mechanism to show values for 9 month period

--- src/run_once.py
+++ src/run_once.py
@@ -3,11 +3,25 @@ from src.optimiser import solve_weights
 
 df = pd.read_csv("data/processed/universe.csv")
 
-w, info = solve_weights(df, month=0)
+"""To show weights for all months (entire 9 month period)"""
+# for i in range(1,10):
+#     for j in range(0,2):
+#         print()
+#     print(f"---------------MONTH {i}---------------")
+#     w, info = solve_weights(df, month=i)
+
+#     out = df[["Equity", "Ticker", "Industry"]].copy()
+#     out["weight"] = w
+#     out = out.sort_values("weight", ascending=True)
+
+#     print(info)
+#     print(out.head(len(df)))
+    
+w, info = solve_weights(df, month=i)
 
 out = df[["Equity", "Ticker", "Industry"]].copy()
 out["weight"] = w
-out = out.sort_values("weight", ascending=False)
+out = out.sort_values("weight", ascending=True)
 
 print(info)
 print(out.head(len(df)))
